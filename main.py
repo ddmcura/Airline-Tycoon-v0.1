9c588b04d5e1f2850020846586a197eb30b67a4e
Daveydave <ddmcura@gmail.com>
2025-07-02 15:00:59 +0800

    added menu page

---
+++ main.py
@@ -0,0 +1,33 @@
+#Main menu
+
+def show_main_menu():
+    print("Welcome to Airline Tycoon")
+    print("1. New Game")
+    print("2. Load Game")
+    print("3. Tutorial")
+    print("4. Settings")
+    print("5. Exit")
+
+def main():
+    while True:
+        show_main_menu()
+        choice = input("Enter Your Choice: ")
+        #NEW GAME
+        if choice == "1":
+            print("Starting a New Game!")
+
+        elif choice == "2":
+            print("Choose a save file from the list below:")
+
+        elif choice == "3":
+            print("Welcome to the Tutorial!")
+        
+        elif choice == "4":
+            print("Settings:")
+
+        elif choice == "5":
+            print("Thank you for playing! Good Bye!")
+            break
+
+
+main()
